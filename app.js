941e162cc97becd2c9d3c05088d5e75403fd8389
manuel <mail@mail.com>
2021-02-02 13:04:18 +0100

    INITIAL

---
+++ app.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const fs = require('fs');
+const port = process.env.PORT || 7000;
+
+const server = http.createServer((req, res)=> {
+    res.writeHead(200, {'Content-Type':'text/html'});
+    var template = fs.readFileSync('./index.html');
+    res.end(template);
+})
+
+server.listen(port);
